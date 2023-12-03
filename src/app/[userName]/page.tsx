33e276ec9dd98f219b517d864dfaf4da62305eb8
niltonslf <niltonrck@gmail.com>
2023-12-03 02:27:40 -0300

    feat: add not found message

--- src/app/[userName]/page.tsx
+++ src/app/[userName]/page.tsx
@@ -15,9 +15,11 @@ const db = getFirestore(app);
 
 export default function UserPage({params}: UserPageProps) {
   const [links, setLinks] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = useCallback(async () => {
-    if (!params.userName) return () => {};
+    if (!params.userName) return setIsLoading(false);
+    setIsLoading(true);
 
     const customQuery = await getDocs(
       collection(db, 'users', params.userName, 'links'),
@@ -25,10 +27,14 @@ export default function UserPage({params}: UserPageProps) {
 
     const res = customQuery.docs;
 
+    if (customQuery.size === 0) return setIsLoading(false);
+
     setLinks([]);
     res.forEach(doc => {
       setLinks(prev => [...prev, doc.data()]);
     });
+
+    setIsLoading(false);
   }, [params.userName]);
 
   useEffect(() => {
@@ -41,6 +47,11 @@ export default function UserPage({params}: UserPageProps) {
         <h2 className='mb-10 flex items-center justify-center text-2xl'>
           {params.userName}
         </h2>
+        {links.length === 0 && isLoading === false && (
+          <div className='rounded-md bg-red-300 p-2 shadow-md'>
+            Profile found
+          </div>
+        )}
         <Link.container>
           {links.length > 0 &&
             links.map(link => {
