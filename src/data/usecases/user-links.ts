9f22ff68863932518498bf75d22acc57c556e770
niltonslf <niltonrck@gmail.com>
2023-12-05 20:34:56 -0300

    fix: deploy build

--- src/data/usecases/user-links.ts
+++
@@ -1,8 +0,0 @@
-import {httpClient} from '@/libs/axios';
-
-export const getUserLinks = async (username: string) => {
-  const res = await httpClient.get(`/${username}`);
-
-  console.log({res});
-  return res;
-};
