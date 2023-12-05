8ea33c690acc40f58390f6d4223554d3f832135a
niltonslf <niltonrck@gmail.com>
2023-12-05 08:40:54 -0300

    test: ensure Link.container and Link.item render its children

---
+++ src/__tests__/app/components/link/index.test.tsx
@@ -0,0 +1,34 @@
+import {User} from 'firebase/auth';
+import * as firestore from 'firebase/firestore';
+
+import {setup} from '@/__tests__/utils';
+import {LinksList} from '@/app/admin/components/links-list';
+import {Link} from '@/app/components';
+import {faker} from '@faker-js/faker';
+import {screen} from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+describe('Link component', () => {
+  it('should render Link.container with a children', () => {
+    const {baseElement} = setup(
+      <Link.container>
+        <span>I'm a child</span>
+      </Link.container>,
+    );
+
+    expect(baseElement.childElementCount).toBe(1);
+    expect(baseElement.children[0].textContent).toBe("I'm a child");
+  });
+
+  it('should render Link.item with a children', () => {
+    const {baseElement} = setup(
+      <Link.item path='/'>
+        <span>I'm a child</span>
+      </Link.item>,
+    );
+
+    expect(baseElement.childElementCount).toBe(1);
+    expect(baseElement.children[0].textContent).toBe("I'm a child");
+  });
+});
