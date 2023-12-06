19f439629f72a95da0dc0ce9b7460848d850fa7f
niltonslf <niltonrck@gmail.com>
2023-12-06 04:48:59 -0300

    feat: add logout feature

--- src/app/admin/components/header/index.tsx
+++ src/app/admin/components/header/index.tsx
@@ -1,27 +1,21 @@
-import {User} from 'firebase/auth';
-import Image from 'next/image';
-import Link from 'next/link';
-import {useEffect, useRef, useState} from 'react';
+import {User, getAuth, signOut} from 'firebase/auth'
+import Image from 'next/image'
+import {useRouter} from 'next/navigation'
 
-import {Dropdown} from '..';
+import {Dropdown} from '..'
 
 type HeaderProps = {
-  user: User;
-};
+  user: User
+}
 
 export const Header = ({user}: HeaderProps) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const dropdown = useRef<HTMLDivElement>(null);
+  const router = useRouter()
 
-  useEffect(() => {
-    function handleClickOutsideBox(event: any) {
-      if (!dropdown?.current?.contains(event.target || null))
-        return setIsOpen(false);
-    }
-
-    document.addEventListener('click', handleClickOutsideBox);
-    return () => removeEventListener('click', handleClickOutsideBox);
-  }, []);
+  const logout = async () => {
+    const auth = getAuth()
+    await signOut(auth)
+    router.push('/')
+  }
 
   return (
     <header className='flex min-w-full flex-row items-center justify-between bg-gray-900 px-5 py-2'>
@@ -40,14 +34,14 @@ export const Header = ({user}: HeaderProps) => {
             />
           )}
         </Dropdown>
-        <Link
-          href='/auth'
-          className='group relative mb-2 me-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-white dark:focus:ring-pink-800'>
+        <div
+          onClick={logout}
+          className='group relative mb-2 me-2 inline-flex cursor-pointer items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-white dark:focus:ring-pink-800'>
           <span className='relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900'>
             logout
           </span>
-        </Link>{' '}
+        </div>{' '}
       </div>
     </header>
-  );
-};
+  )
+}
