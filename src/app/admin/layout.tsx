ab731316c78f78eb4654fbcafb199077ec2458e1
niltonslf <niltonrck@gmail.com>
2023-12-06 06:13:19 -0300

    fix: auth login check

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,16 +4,21 @@ import {getAuth} from 'firebase/auth'
 import {useRouter} from 'next/navigation'
 import {ReactNode, useEffect} from 'react'
 
+import {app} from '@/libs/firebase'
+
 type AdminLayoutProps = {
   children: ReactNode
 }
+
+const auth = getAuth(app)
 
 export default function AdminLayout({children}: AdminLayoutProps) {
   const router = useRouter()
 
   useEffect(() => {
-    const auth = getAuth()
-    if (!auth.currentUser) router.push('/auth')
+    setTimeout(() => {
+      if (!auth.currentUser?.uid) return router.push('/auth')
+    }, 2000)
   }, [router])
 
   return children
