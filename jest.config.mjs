44bfb3bdbe3528b85a383fd742def72edd101cc3
niltonslf <niltonrck@gmail.com>
2023-12-08 03:10:48 -0300

    feat: remove mocks folder from tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,13 +1,13 @@
-import nextJest from 'next/jest.js';
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   dir: './',
-});
+})
 
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'jest-environment-jsdom',
-  modulePathIgnorePatterns: ['(/utils/.*)$'],
-};
+  modulePathIgnorePatterns: ['(/utils/.*)$', '(/mocks/.*)$'],
+}
 
-export default createJestConfig(config);
+export default createJestConfig(config)
