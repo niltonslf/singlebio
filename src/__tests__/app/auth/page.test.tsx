aed4732b6eacbff06ffc925aa5e22fb9e8d15dcd
niltonslf <niltonrck@gmail.com>
2023-12-08 23:13:45 -0300

    tests: WIP

--- src/__tests__/app/auth/page.test.tsx
+++ src/__tests__/app/auth/page.test.tsx
@@ -1,73 +1,93 @@
-import '@testing-library/jest-dom';
-import * as auth from 'firebase/auth';
-import * as firestore from 'firebase/firestore';
-import mockRouter from 'next-router-mock';
+import '@testing-library/jest-dom'
+import * as auth from 'firebase/auth'
+import * as firestore from 'firebase/firestore'
+import mockRouter from 'next-router-mock'
 
-import {setup} from '@/__tests__/utils';
-import AuthPage from '@/app/auth/page';
-import {faker} from '@faker-js/faker';
-import {screen, render} from '@testing-library/react';
+import {setup} from '@/__tests__/utils'
+import {makeFbUser, makeUser} from '@/__tests__/utils/mocks'
+import {authStore} from '@/app/auth/context/auth-state'
+import AuthPage from '@/app/auth/page'
+import {screen, render} from '@testing-library/react'
 
-jest.mock('next/navigation', () => jest.requireActual('next-router-mock'));
+jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
 
 jest.mock('firebase/firestore', () => {
-  return {__esModule: true, ...jest.requireActual('firebase/firestore')};
-});
+  return {__esModule: true, ...jest.requireActual('firebase/firestore')}
+})
 
 jest.mock('firebase/auth', () => {
-  return {__esModule: true, ...jest.requireActual('firebase/auth')};
-});
+  return {__esModule: true, ...jest.requireActual('firebase/auth')}
+})
+
+jest.mock('@/app/auth/context/auth-state.ts', () => ({
+  __esModule: true,
+  ...jest.requireActual('@/app/auth/context/auth-state.ts'),
+}))
+
+/**
+ * Generators
+ */
+
+export const validateGoogleBtn = () => {
+  const googleButton = screen.getByRole('button')
+  expect(googleButton.textContent).toBe('Sign up with Google')
+
+  return googleButton
+}
 
 describe('Auth Page', () => {
   it('render Auh page with the login button', () => {
-    render(<AuthPage />);
+    render(<AuthPage />)
 
-    const formTitle = screen.getByText('SignIn');
-    expect(formTitle.textContent).toBe('SignIn');
+    const formTitle = screen.getByText('SignIn')
+    expect(formTitle.textContent).toBe('SignIn')
 
-    const googleButton = screen.getByRole('button');
-    expect(googleButton.textContent).toBe('Sign up with Google');
-  });
+    validateGoogleBtn()
+  })
 
   it('Should return an error when clicked to Login with Google', async () => {
-    jest.spyOn(auth, 'signInWithPopup').mockRejectedValue('error');
+    jest.spyOn(auth, 'signInWithPopup').mockRejectedValue('error')
 
-    const {user} = setup(<AuthPage />);
+    const {user} = setup(<AuthPage />)
 
-    const googleButton = screen.getByRole('button');
-    expect(googleButton.textContent).toBe('Sign up with Google');
+    const googleButton = validateGoogleBtn()
 
-    await user.click(googleButton);
+    await user.click(googleButton)
 
     const errorBox = await screen.getByText(
       'There was an error to access your account. Please, try again later or use a different account.',
-    );
-    expect(errorBox).toBeVisible();
-  });
+    )
 
-  it('Should login with success when clicked at Login with Google', async () => {
-    jest.spyOn(auth, 'signInWithPopup').mockResolvedValue({
-      user: {
-        uid: faker.word.words(1),
-        displayName: faker.person.fullName,
-        email: faker.internet.email,
-        photoURL: faker.image.urlLoremFlickr,
-      },
-    } as any);
+    expect(errorBox).toBeVisible()
+  })
 
-    jest.spyOn(firestore, 'setDoc').mockImplementation(() => jest.fn() as any);
+  it('Should Login with Google successfully  ', async () => {
+    jest
+      .spyOn(auth, 'signInWithPopup')
+      .mockResolvedValue({user: makeFbUser()} as any)
 
-    const {user} = setup(<AuthPage />);
+    jest.spyOn(authStore, 'fetchFirebaseUser').mockResolvedValue({
+      user: makeUser(),
+      exists: true,
+    })
 
-    const googleButton = screen.getByRole('button');
-    expect(googleButton.textContent).toBe('Sign up with Google');
+    jest.spyOn(firestore, 'doc').mockImplementation(jest.fn())
 
-    await user.click(googleButton);
+    jest.spyOn(firestore, 'getDoc').mockResolvedValueOnce({
+      data: () => makeFbUser(),
+      exists: true,
+    } as any)
+
+    const {user} = setup(<AuthPage />)
+
+    const googleButton = validateGoogleBtn()
+
+    await user.click(googleButton)
 
     expect(mockRouter).toMatchObject({
       asPath: '/admin',
       pathname: '/admin',
       query: {},
-    });
-  });
-});
+    })
+  })
+})
