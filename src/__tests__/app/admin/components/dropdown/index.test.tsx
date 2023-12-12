384976b951dd75694968d11315a4113d5874c9d5
niltonslf <niltonrck@gmail.com>
2023-12-12 19:04:54 -0300

    test: ensure dropdown will open

--- src/__tests__/app/admin/components/dropdown/index.test.tsx
+++ src/__tests__/app/admin/components/dropdown/index.test.tsx
@@ -1,6 +1,7 @@
 import {Dropdown} from '@/app/admin/components'
 import '@testing-library/jest-dom'
 import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('Dropdown', () => {
   it('should render component', async () => {
@@ -18,7 +19,28 @@ describe('Dropdown', () => {
     expect(menuContainer.children).toHaveLength(2)
   })
 
-  it.todo('should open dropdown')
+  it('should open dropdown', async () => {
+    const user = userEvent.setup()
+
+    render(
+      <Dropdown>
+        <button>open dropdown</button>
+      </Dropdown>,
+    )
+
+    const triggerItem = screen.getByRole('button')
+    const menuContainer = screen.getByRole('list')
+    let hasHidden = menuContainer.parentElement?.classList.contains('hidden')
+
+    expect(hasHidden).toBe(true)
+
+    await user.click(triggerItem)
+
+    hasHidden = menuContainer.parentElement?.classList.contains('hidden')
+
+    expect(hasHidden).toBe(false)
+    expect(menuContainer.children).toHaveLength(2)
+  })
 
   it.todo('should close menu when clicked outside')
 
