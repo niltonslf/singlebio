08f2618c2743fa753b0fa4ac162c9a2731df9037
niltonslf <niltonrck@gmail.com>
2023-12-12 20:01:06 -0300

    test: ensure first item has a link

--- src/__tests__/app/admin/components/dropdown/index.test.tsx
+++ src/__tests__/app/admin/components/dropdown/index.test.tsx
@@ -1,69 +1,68 @@
+import {makeUser} from '@/__tests__/utils'
 import {Dropdown} from '@/app/admin/components'
+import {authStore} from '@/app/auth/context/auth-store'
 import '@testing-library/jest-dom'
 import {cleanup, render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
+const makeSUT = () => {
+  return render(
+    <Dropdown>
+      <button>open dropdown</button>
+    </Dropdown>,
+  )
+}
+
 const handleMenuContainer = () => {
-  const menuContainer = screen.getByRole('list')
-  let hasHidden = menuContainer.parentElement?.classList.contains('hidden')
+  const menuList = screen.getByRole('list')
+  let hasHidden = menuList.parentElement?.classList.contains('hidden')
 
   return {
     hasHidden,
-    menuContainer,
+    menuList,
   }
 }
 
 describe('Dropdown', () => {
   beforeEach(() => {
     cleanup()
+    authStore.clearUser()
   })
 
   it('should render component', async () => {
-    render(
-      <Dropdown>
-        <button>open dropdown</button>
-      </Dropdown>,
-    )
+    makeSUT()
 
     const triggerItem = screen.getByRole('button')
-    const menuContainer = screen.getByRole('list')
+    const menuList = screen.getByRole('list')
 
     expect(triggerItem).toBeInTheDocument()
-    expect(menuContainer).toBeInTheDocument()
-    expect(menuContainer.children).toHaveLength(2)
+    expect(menuList).toBeInTheDocument()
+    expect(menuList.children).toHaveLength(2)
   })
 
   it('should open dropdown', async () => {
     const user = userEvent.setup()
 
-    render(
-      <Dropdown>
-        <button>open dropdown</button>
-      </Dropdown>,
-    )
+    makeSUT()
 
     const triggerItem = screen.getByRole('button')
-    const menuContainer = screen.getByRole('list')
-    let hasHidden = menuContainer.parentElement?.classList.contains('hidden')
+    const menuList = screen.getByRole('list')
+    let hasHidden = menuList.parentElement?.classList.contains('hidden')
 
     expect(hasHidden).toBe(true)
 
     await user.click(triggerItem)
 
-    hasHidden = menuContainer.parentElement?.classList.contains('hidden')
+    hasHidden = menuList.parentElement?.classList.contains('hidden')
 
     expect(hasHidden).toBe(false)
-    expect(menuContainer.children).toHaveLength(2)
+    expect(menuList.children).toHaveLength(2)
   })
 
   it('should close menu when clicked outside', async () => {
     const user = userEvent.setup()
 
-    const {baseElement} = render(
-      <Dropdown>
-        <button>open dropdown</button>
-      </Dropdown>,
-    )
+    const {baseElement} = makeSUT()
 
     const triggerItem = screen.getByRole('button')
     const {hasHidden} = handleMenuContainer()
@@ -71,10 +70,10 @@ describe('Dropdown', () => {
     expect(hasHidden).toBe(true)
 
     await user.click(triggerItem)
-    const {hasHidden: hasHidden2, menuContainer} = handleMenuContainer()
+    const {hasHidden: hasHidden2, menuList} = handleMenuContainer()
 
     expect(hasHidden2).toBe(false)
-    expect(menuContainer.children).toHaveLength(2)
+    expect(menuList.children).toHaveLength(2)
 
     await user.click(baseElement)
     const {hasHidden: hasHidden3} = handleMenuContainer()
@@ -82,7 +81,24 @@ describe('Dropdown', () => {
     expect(hasHidden3).toBe(true)
   })
 
-  it.todo('should go to link')
+  it('should the first item has the correct link', async () => {
+    const user = userEvent.setup()
+
+    authStore.user = makeUser()
+    const path = `/${authStore.user.userName}`
+
+    makeSUT()
+
+    const {menuList} = handleMenuContainer()
+
+    // select first li
+    const firstItem = menuList.children[0]
+    const link = firstItem.firstElementChild
+
+    if (!link) return fail()
+
+    expect(link).toHaveAttribute('href', path)
+  })
 
   it.todo('should call deleteAccount function')
 })
