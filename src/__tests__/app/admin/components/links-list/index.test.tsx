71ddbdbcaf7af3c6efeb8027fe594b2ae7e51e60
niltonslf <niltonrck@gmail.com>
2023-12-12 22:46:02 -0300

    refactor: simplifying tests

--- src/__tests__/app/admin/components/links-list/index.test.tsx
+++ src/__tests__/app/admin/components/links-list/index.test.tsx
@@ -1,77 +1,62 @@
-import {User} from 'firebase/auth';
-import * as firestore from 'firebase/firestore';
+import * as firestore from 'firebase/firestore'
 
-import {setup} from '@/__tests__/utils';
-import {LinksList} from '@/app/admin/components/links-list';
-import {faker} from '@faker-js/faker';
-import {screen} from '@testing-library/react';
+import {makeUser, setup} from '@/__tests__/utils'
+import {LinksList} from '@/app/admin/components/links-list'
+import {faker} from '@faker-js/faker'
+import {cleanup, screen} from '@testing-library/react'
 
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
 
 jest.mock('firebase/firestore', () => {
-  return {__esModule: true, ...jest.requireActual('firebase/firestore')};
-});
+  return {__esModule: true, ...jest.requireActual('firebase/firestore')}
+})
 
 describe('Links List component', () => {
+  beforeEach(() => {
+    cleanup()
+  })
+
   it('render component empty', () => {
-    const userMock = {
-      displayName: faker.person.fullName(),
-      photoURL: faker.image.urlLoremFlickr(),
-    } as User;
+    const userMock = makeUser()
 
-    setup(<LinksList user={userMock} />);
+    setup(<LinksList user={userMock} />)
 
-    const list = screen.getByRole('list');
-    expect(list.childElementCount).toBe(0);
-  });
+    const list = screen.getByRole('list')
+    expect(list.childElementCount).toBe(0)
+  })
 
   it('render component with 2 items', () => {
-    const userMock = {
-      uid: faker.string.uuid(),
-      displayName: faker.person.fullName(),
-      photoURL: faker.image.urlLoremFlickr(),
-    } as User;
+    const userMock = makeUser()
 
-    const linkMock = (label?: string, url?: string) => {
+    const makeLink = (label?: string, url?: string) => {
       return {
         label: label || faker.word.words(2),
         url: url || faker.internet.url(),
-      };
-    };
+      }
+    }
 
-    const linkMockCompareData = linkMock('instagram', 'http://instagram.com');
+    const linksMock = [
+      makeLink('instagram', 'http://instagram.com'),
+      makeLink(),
+    ]
 
-    const callbackMockResponse = [
-      {
-        data() {
-          return linkMockCompareData;
-        },
-      },
-      {
-        data() {
-          return linkMock();
-        },
-      },
-    ];
+    const responseMock = linksMock.map(link => ({data: () => link}))
 
     jest
       .spyOn(firestore, 'onSnapshot')
       .mockImplementation((query: any, callback: any) => {
-        callback(callbackMockResponse);
-        return jest.fn();
-      });
+        callback(responseMock)
+        return jest.fn()
+      })
 
-    setup(<LinksList user={userMock} />);
+    setup(<LinksList user={userMock} />)
 
-    const list = screen.getByRole('list');
-    expect(list.childElementCount).toBe(2);
+    const list = screen.getByRole('list')
 
-    const firstItem = list.children[1];
+    const firstItem = list.children[1]
 
-    expect(firstItem.querySelector('span')?.textContent).toBe(
-      linkMockCompareData.url,
-    );
-
-    expect(firstItem.textContent).toContain(linkMockCompareData.label);
-  });
-});
+    expect(list.childElementCount).toBe(2)
+    expect(firstItem.querySelector('span')?.textContent).toBe(linksMock[0].url)
+    expect(firstItem.textContent).toContain(linksMock[0].label)
+  })
+})
