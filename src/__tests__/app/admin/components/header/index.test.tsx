e149ade09bbd37b4db9bb9ac9091fc85c4a98093
niltonslf <niltonrck@gmail.com>
2023-12-12 22:14:40 -0300

    test: ensure login methods are being called

--- src/__tests__/app/admin/components/header/index.test.tsx
+++ src/__tests__/app/admin/components/header/index.test.tsx
@@ -1,19 +1,42 @@
-import 'next-router-mock'
+import * as firebaseAuth from 'firebase/auth'
+import routerMock from 'next-router-mock'
 
 import {setup} from '@/__tests__/utils'
-import {Header} from '@/app/admin/components/header'
-import {screen} from '@testing-library/react'
-
 import '@testing-library/jest-dom'
 import {makeUser} from '@/__tests__/utils/mocks'
+import {Header} from '@/app/admin/components/header'
+import {authStore} from '@/app/auth/context/auth-store'
+import {User} from '@/models'
+import {cleanup, screen} from '@testing-library/react'
+
+import {act} from 'react-dom/test-utils'
 
 jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
 
-describe('Header component', () => {
-  it('render component with logo and user data', () => {
-    const userMock = makeUser()
+jest.mock('firebase/auth', () => ({
+  __esModule: true,
+  ...jest.requireActual('firebase/auth'),
+}))
 
-    setup(<Header user={userMock} />)
+const makeSUT = (user?: User) => {
+  const userMock = user ?? makeUser()
+
+  const sut = setup(<Header user={userMock} />)
+
+  return {
+    userMock,
+    ...sut,
+  }
+}
+
+describe('Header component', () => {
+  beforeEach(() => {
+    cleanup()
+    authStore.clearUser()
+  })
+
+  it('render component with logo and user data', () => {
+    const {userMock} = makeSUT()
 
     const header = screen.getByText('Lnktree admin')
     expect(header).toBeInTheDocument
@@ -25,10 +48,9 @@ describe('Header component', () => {
     expect(profilePicture).toBeInTheDocument
   })
 
+  // ? Check if this tests is really necessary
   it('render component without user image', () => {
-    const userMock = makeUser()
-
-    setup(<Header user={userMock} />)
+    const {userMock} = makeSUT()
 
     const header = screen.getByText('Lnktree admin')
     expect(header).toBeInTheDocument
@@ -40,10 +62,9 @@ describe('Header component', () => {
     expect(profilePicture).not.toBeInTheDocument
   })
 
+  // ? Check if this tests is really necessary
   it('render component without user name', () => {
-    const userMock = makeUser(undefined, '')
-
-    setup(<Header user={userMock} />)
+    makeSUT(makeUser(undefined, ''))
 
     const header = screen.getByText('Lnktree admin')
     expect(header).toBeInTheDocument
@@ -53,5 +74,29 @@ describe('Header component', () => {
 
     const profilePicture = screen.getByRole('img')
     expect(profilePicture).toBeInTheDocument()
+  })
+
+  it('should logout user', async () => {
+    routerMock.push('/admin')
+
+    jest.spyOn(firebaseAuth, 'getAuth').mockImplementation()
+    jest.spyOn(firebaseAuth, 'signOut').mockImplementation()
+    jest.spyOn(routerMock, 'push')
+
+    const {userMock, user} = makeSUT()
+    // fake login
+    await act(() => {
+      authStore.updateUser(userMock)
+    })
+
+    const logoutBtn = screen.getByText(/logout/i)
+    expect(logoutBtn).toBeVisible()
+
+    await user.click(logoutBtn)
+
+    expect(firebaseAuth.getAuth).toHaveBeenCalledTimes(1)
+    expect(firebaseAuth.signOut).toHaveBeenCalledTimes(1)
+    expect(routerMock.push).toHaveBeenCalledTimes(1)
+    expect(routerMock.pathname).toBe('/')
   })
 })
