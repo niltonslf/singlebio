a6c272463c0a21da3317ebe148dc044e11bfb3ec
niltonslf <niltonrck@gmail.com>
2023-12-12 21:24:10 -0300

    refactor: change condition about what to render depening on menu item props

--- src/app/admin/components/dropdown/menu-item.tsx
+++ src/app/admin/components/dropdown/menu-item.tsx
@@ -7,15 +7,23 @@ interface MenuItemProps {
 }
 
 export const MenuItem = ({children, href, onClick}: MenuItemProps) => {
+  const styles =
+    'block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
+
+  if (href)
+    return (
+      <li>
+        <a href={href} target={onClick ? '_self' : '_blank'} className={styles}>
+          {children}
+        </a>
+      </li>
+    )
+
   return (
     <li>
-      <a
-        href={href || '#'}
-        target={onClick ? '_self' : '_blank'}
-        onClick={onClick || (e => null)}
-        className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
+      <div onClick={onClick} className={styles}>
         {children}
-      </a>
+      </div>
     </li>
   )
 }
