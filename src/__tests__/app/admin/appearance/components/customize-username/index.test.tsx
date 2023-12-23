f320bbf35f72954bed0a2561b40654ef4a82cf7c
niltonslf <niltonrck@gmail.com>
2023-12-23 04:04:57 -0300

    test: allow user choose the username color

---
+++ src/__tests__/app/admin/appearance/components/customize-username/index.test.tsx
@@ -0,0 +1,33 @@
+import {fail, setup} from '@/__tests__/utils'
+import {CustomizeUsername} from '@/app/admin/appearance/components'
+import {appearanceStore} from '@/app/admin/appearance/context'
+import '@testing-library/jest-dom'
+import {cleanup, waitFor} from '@testing-library/react'
+
+const makeSUT = () => {
+  return setup(<CustomizeUsername />)
+}
+
+describe('Customize username', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('should select username color', async () => {
+    const {user} = makeSUT()
+
+    const colorPicker = document.querySelector('div[aria-label=Color]')
+
+    if (!colorPicker) return fail()
+
+    await user.pointer({
+      target: colorPicker,
+      coords: {clientX: 100, clientY: 50},
+      keys: '[MouseLeft]',
+    })
+
+    await waitFor(() => {
+      expect(appearanceStore.theme.usernameColor).not.toBe('')
+    })
+  })
+})
