ca30d100a6acbf1bb0626fa202ba86f092885657
niltonslf <niltonrck@gmail.com>
2023-12-30 08:10:10 -0300

    feat: add feature to delete account

--- src/app/admin/components/dropdown/index.tsx
+++ src/app/admin/components/dropdown/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {LogOut, Trash} from 'lucide-react'
+import {LogOut} from 'lucide-react'
 import {observer} from 'mobx-react-lite'
 import {PropsWithChildren} from 'react'
 
@@ -15,25 +15,14 @@ export const Dropdown = observer(({children}: DropdownProps) => {
     return authStore.logout()
   }
 
-  const deleteAccount = async () => {
-    await authStore.deleteUser()
-  }
-
   return (
     <div className='background dropdown' data-testid='header-dropdown'>
       <label className='cursor-pointer' tabIndex={0}>
         {children}
       </label>
       <div className='solid menu bottom-left !w-48'>
-        <span className='item text-sm text-primary-700' onClick={deleteAccount}>
-          <Trash size={15} />
-          <p>Delete account</p>
-        </span>
-
-        <div className='is-divider' role='separator'></div>
-
         <span className='px-3 py-2'>
-          <button onClick={logout} className='bw btn md solid w-full'>
+          <button onClick={logout} className='bw btn outline md w-full'>
             <p>Logout</p>
             <LogOut size={18} />
           </button>
--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -2,58 +2,51 @@
 
 import {observer} from 'mobx-react-lite'
 
+import {authStore} from '@/app/auth/context/auth-store'
 import {Button} from '@/app/components'
 
-import {AdminBaseLayout} from '../components'
+import {AdminBaseLayout, SectionCard} from '../components'
 
 export const Settings = observer(() => {
+  const deleteAccount = async () => {
+    await authStore.deleteUser()
+  }
+
   return (
     <AdminBaseLayout>
       <AdminBaseLayout.Content>
         <h1 className='mb-8 text-2xl font-semibold'>Settings</h1>
-
-        <div className='flex w-full flex-col gap-2 overflow-hidden rounded-lg bg-background-300 p-6 shadow-lg'>
-          <div className='mb-5 flex items-baseline'>
-            <h3 className='text-xl font-semibold'>Language</h3>
-          </div>
-          <div className='w-full'>
+        <div className='flex flex-col gap-5'>
+          <SectionCard title='Language'>
             <div className='background border-background-600'>
               <select className='bw solid select'>
                 <option>English</option>
-                <option>Option 2</option>
-                <option>Option 3</option>
               </select>
             </div>
-          </div>
-        </div>
-        {/*  */}
+          </SectionCard>
+          {/*  */}
 
-        <div className='mt-5 flex w-full flex-col gap-2 overflow-hidden rounded-lg bg-background-300 p-6 shadow-lg'>
-          <div className='mb-5 flex items-baseline'>
-            <h3 className='text-xl font-semibold'>App theme</h3>
-          </div>
-          <div className='w-full'>
+          <SectionCard title='App theme'>
             <div className='background border-background-600'>
               <select className='bw solid select'>
                 <option>Dark theme</option>
-                <option>Option 2</option>
               </select>
             </div>
-          </div>
-        </div>
-        {/*  */}
-        <div className='mt-5 flex w-full flex-col gap-2 overflow-hidden rounded-lg bg-background-300 p-6 shadow-lg'>
-          <div className='mb-5 flex items-baseline'>
-            <h3 className='text-xl font-semibold'>Delete Account</h3>
-          </div>
-          <div>
-            <div className='warn prompt mb-5 border-warn-600'>
-              <p className='content'>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              </p>
+          </SectionCard>
+          {/*  */}
+          <SectionCard title='Delete Account'>
+            <div>
+              <div className='danger sm prompt mb-10 border-warn-600 !bg-red-400 !text-white'>
+                <p className='content'>
+                  Be aware that this action will delete all your data and it's
+                  not possible to come back.
+                </p>
+              </div>
+              <Button variant='error' onClick={() => deleteAccount()}>
+                Delete account
+              </Button>
             </div>
-            <Button variant='error'>Delete account</Button>
-          </div>
+          </SectionCard>
         </div>
       </AdminBaseLayout.Content>
     </AdminBaseLayout>
