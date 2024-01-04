41636f58413561537e2287b5d6170f996d421f26
niltonslf <niltonrck@gmail.com>
2024-01-03 22:11:32 -0300

    feat: mobile home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,14 @@ export default function Home() {
           href='/auth'
           className='group relative mb-2 me-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-medium text-gray-900 hover:text-neutral-50 focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-neutral-50 dark:focus:ring-pink-800'>
           <span className='relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900'>
-            Access
+            Login
           </span>
         </Link>
       </nav>
 
-      <section className='mx-auto mt-14 flex w-full max-w-6xl flex-row items-center justify-center'>
-        <article className='w-[55%] '>
-          <h1 className='text-left text-7xl  font-extrabold leading-none'>
+      <section className='max:w-full mx-auto mt-14 flex w-full flex-row items-center justify-center md:max-w-6xl'>
+        <article className='w-full text-center md:w-[55%] md:text-left'>
+          <h1 className='text-4xl font-extrabold  leading-none md:text-7xl'>
             <span className='bg-gradient-to-r from-fuchsia-600 to-pink-600 bg-clip-text text-transparent'>
               Share
             </span>{' '}
@@ -35,7 +35,7 @@ export default function Home() {
             </span>
           </h1>
 
-          <p className='mb-10 mt-5 w-[80%] text-lg font-normal text-neutral-200'>
+          <p className='mb-10 mt-5 w-full text-lg font-normal text-neutral-200 md:w-[80%]'>
             Compile all your links into one, customized page and connect with
             your audience seamlessly.
           </p>
@@ -48,7 +48,7 @@ export default function Home() {
 
         <div className='w-35% ml-auto flex h-full flex-row items-center justify-center'>
           <div className='overflow-hidden rounded-2xl '>
-            <Image src='/model.png' alt='model' width={300} height={700} />
+            {/* <Image src='/model.png' alt='model' width={300} height={700} /> */}
           </div>
         </div>
       </section>
