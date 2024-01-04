7a67d344730fa61f4362f16cf4236ffe534da720
niltonslf <niltonrck@gmail.com>
2024-01-04 13:10:48 -0300

    feat: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className='via-background-200 flex h-[100vh] w-screen flex-col bg-gradient-to-tr from-base-100 to-neutral p-5'>
+    <main className='via-background-200 flex h-[100vh] w-screen flex-col bg-gradient-to-tr from-base-100 via-base-300 to-base-100 p-5'>
       <nav className='mx-auto flex w-full max-w-6xl items-center justify-between py-2'>
         <Link href='/' title='Brand' className='font-semibold'>
           <Image
@@ -14,12 +14,8 @@ export default function Home() {
           />
         </Link>
 
-        <Link
-          href='/auth'
-          className='group relative mb-2 me-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-medium text-gray-900 hover:text-neutral-50 focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-neutral-50 dark:focus:ring-pink-800'>
-          <span className='relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900'>
-            Login
-          </span>
+        <Link href='/auth' className='btn btn-outline btn-neutral px-10'>
+          Login
         </Link>
       </nav>
 
@@ -39,16 +35,14 @@ export default function Home() {
             Compile all your links into one, customized page and connect with
             your audience seamlessly.
           </p>
-          <Link
-            href='/auth'
-            className='btn border-pink-500 bg-gradient-to-tr from-pink-500 to-orange-400 !py-3 text-base'>
+          <Link href='/auth' className='btn bg-primary !py-3 text-base'>
             Create my page
           </Link>
         </article>
 
         <div className='w-35% ml-auto flex h-full flex-row items-center justify-center'>
-          <div className='overflow-hidden rounded-2xl '>
-            {/* <Image src='/model.png' alt='model' width={300} height={700} /> */}
+          <div className='rotate-[7deg] rounded-2xl '>
+            <Image src='/home/model.png' alt='model' width={300} height={700} />
           </div>
         </div>
       </section>
