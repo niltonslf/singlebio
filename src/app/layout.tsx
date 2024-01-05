ec831790499270be5e0ba79c32eb482a91bd6c86
niltonslf <niltonrck@gmail.com>
2024-01-05 07:45:20 -0300

    feat: add metadata to the home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,51 @@ import {Inter} from 'next/font/google'
 
 const inter = Inter({subsets: ['latin']})
 
+const baseUrl = process.env.NEXT_PUBLIC_APP_URL
+
 export const metadata: Metadata = {
-  title: 'Lnktree',
-  description: 'Share your links with your followers.',
+  title: 'Lnktree | Share your links',
+  description: 'Share your digital world with just a single link!',
+
+  metadataBase: new URL(baseUrl || ''),
+  openGraph: {
+    images: [
+      {
+        url: '/home/model.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/home/model.png',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  icons: {
+    icon: '/logo-icon-black.png',
+    shortcut: '/logo-icon-black.png',
+    apple: '/logo-icon-black.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/logo-icon-black.png',
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      'index': true,
+      'follow': false,
+      'noimageindex': false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
