c732b6eab2f9fe3f51e740b248035ba6b9215cfc
niltonslf <niltonrck@gmail.com>
2024-01-18 18:05:25 -0300

    feat: add button in the verify email to go back to auth

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {AlertTriangle} from 'lucide-react'
+import Link from 'next/link'
 import {useEffect} from 'react'
 
 import {authStore} from '../context/auth-store'
@@ -11,10 +12,15 @@ const VerifyEmailPage = () => {
   }, [])
 
   return (
-    <div className='alert alert-warning'>
-      <AlertTriangle />
-      <p>Please, check your inbox in order to verify your email.</p>
-    </div>
+    <>
+      <div className='alert alert-warning'>
+        <AlertTriangle />
+        <p>Please, check your inbox in order to verify your email.</p>
+      </div>
+      <Link href='/auth' className='btn btn-primary mt-5'>
+        Go back
+      </Link>
+    </>
   )
 }
 
