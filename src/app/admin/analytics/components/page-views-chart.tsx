68c3268f3f60d68e628c461e5ef801ce3b8b8c3f
niltonslf <niltonrck@gmail.com>
2024-01-21 23:23:44 -0300

    refactor: remove apex charts

---
+++ src/app/admin/analytics/components/page-views-chart.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import {observer} from 'mobx-react-lite'
+import {useCallback, useEffect} from 'react'
+
+import {authStore} from '@/app/auth/context/auth-store'
+
+import {adminAnalyticsStore} from '../context/admin-analytics-store'
+
+export const PageViewsChart = observer(() => {
+  const user = authStore.user
+
+  const fetchData = useCallback(async () => {
+    await adminAnalyticsStore.fetchPageViews()
+  }, [])
+
+  useEffect(() => {
+    adminAnalyticsStore.setUser(user)
+    fetchData()
+  }, [fetchData, user])
+
+  return <div>Under development...</div>
+})
--- src/app/admin/analytics/page.tsx
+++ src/app/admin/analytics/page.tsx
@@ -1,49 +1,11 @@
 'use client'
 
 import {observer} from 'mobx-react-lite'
-import {useCallback, useEffect, useState} from 'react'
-import Chart from 'react-apexcharts'
-
-import {authStore} from '@/app/auth/context/auth-store'
-import {PageViewsChartData} from '@/domain/models'
 
 import {SectionCard} from '../components'
-import {adminAnalyticsStore} from './context/admin-analytics-store'
+import {PageViewsChart} from './components'
 
 const AnalyticsPage = observer(() => {
-  const user = authStore.user
-
-  const [pageViewsData, setPageViewsData] =
-    useState<PageViewsChartData | null>()
-
-  const pageViewsChartOptions: ApexCharts.ApexOptions = {
-    chart: {id: 'area', toolbar: {show: false}},
-    xaxis: {
-      categories: pageViewsData?.categories ?? [],
-    },
-    tooltip: {
-      theme: 'dark',
-    },
-  }
-
-  const pageViewsChartData: ApexAxisChartSeries = [
-    {
-      name: 'Views',
-      data: pageViewsData?.data ?? [],
-      type: 'area',
-    },
-  ]
-
-  const fetchData = useCallback(async () => {
-    const data = await adminAnalyticsStore.fetchPageViews()
-    setPageViewsData(data)
-  }, [])
-
-  useEffect(() => {
-    adminAnalyticsStore.setUser(user)
-    fetchData()
-  }, [fetchData, user])
-
   return (
     <div className='mt-5 flex flex-wrap gap-5'>
       <div className='flex w-full justify-end'>
@@ -65,18 +27,13 @@ const AnalyticsPage = observer(() => {
       </div>
 
       <div className='flex flex-1 gap-5'>
-        <SectionCard title='Links'>Here</SectionCard>
-        <SectionCard title='Social'>Here</SectionCard>
+        <SectionCard title='Links'>Under development...</SectionCard>
+        <SectionCard title='Social'>Under development...</SectionCard>
       </div>
 
       <div className='w-full '>
         <SectionCard title='Page views'>
-          <Chart
-            options={pageViewsChartOptions}
-            series={pageViewsChartData}
-            width='100%'
-            height='250'
-          />
+          <PageViewsChart />
         </SectionCard>
       </div>
     </div>
