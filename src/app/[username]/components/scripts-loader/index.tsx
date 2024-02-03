ff5c1c537f393d69df28379848706aff3928b407
niltonslf <niltonrck@gmail.com>
2024-02-02 21:05:32 -0300

    feat: add script to load google analytics in the user page

--- src/app/[username]/components/scripts-loader/index.tsx
+++ src/app/[username]/components/scripts-loader/index.tsx
@@ -1,29 +1,5 @@
-import Script from 'next/script'
+import {GoogleAnalytics} from '@next/third-parties/google'
 
 export const ScriptsLoader = ({gaCode}: {gaCode?: string}) => {
-  return (
-    <>
-      {gaCode && (
-        <>
-          <Script
-            strategy='afterInteractive'
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaCode}`}
-          />
-
-          <Script
-            id='user-page-analytics'
-            strategy='afterInteractive'
-            dangerouslySetInnerHTML={{
-              __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaCode}');
-          `,
-            }}
-          />
-        </>
-      )}
-    </>
-  )
+  return <>{gaCode && <GoogleAnalytics gaId={gaCode} />}</>
 }
